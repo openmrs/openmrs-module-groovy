359ec6e21c2178d3bde552f38e49c68f45e6dd87
Robby O'Connor <robby.oconnor@gmail.com>
2009-05-14 08:40:36 +0000

    groovy module: add the js files

---
+++ web/module/resources/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $(function() {
+        $("#tabs").tabs();
+        $("#textarea-container").resizable({ handles: 's', alsoResize: 'iframe' });
+    });
+
+    $("#executeButton").click(function(event) {
+        var script = editor.getCode();
+        DWRGroovyService.eval(script, function(data) {
+            $('#output').html("");
+            $('#result').html("");
+            $('#stacktrace').html("");
+
+            if (data[1] != "") {
+                $("#tabs").tabs('select', 1);
+                $('#output').html(data[1]).fadeIn();
+            } else {
+                $('#output').fadeOut();
+            }
+
+            if (data[0] != "null") {
+                $("#tabs").tabs('select', 0);
+                $('#result').html(data[0]).fadeIn();
+            } else {
+                $('#result').fadeOut();
+            }
+            if (data[2] != "") {
+                $("#tabs").tabs('select', 2);
+                $('#stacktrace').html(data[2]).fadeIn();
+            } else {
+                $('#stacktrace').fadeOut();
+            }
+        });
+    });
+});
