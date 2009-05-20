bcfefecdfb73fc0d2c1cc97ee598714d5f9237d5
Robby O'Connor <robby.oconnor@gmail.com>
2009-05-20 18:53:33 +0000

    groovy module: tweak on the validator.

--- src/org/openmrs/module/groovy/validators/GroovyScriptValidator.groovy
+++ src/org/openmrs/module/groovy/validators/GroovyScriptValidator.groovy
@@ -1,3 +1,16 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 package org.openmrs.module.groovy.validators;
 
 import org.springframework.validation.Errors;
@@ -13,7 +26,7 @@ public class GroovyScriptValidator implements Validator {
   }
 
   public void validate(Object obj, Errors errors) {
-    ValidationUtils.rejectIfEmpty(errors, "name", "groovy.name.error");
+    ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "groovy.name.error");
     ValidationUtils.rejectIfEmptyOrWhitespace(errors, "script", "groovy.script.error");
     
     GroovyScript script = (GroovyScript) obj;
