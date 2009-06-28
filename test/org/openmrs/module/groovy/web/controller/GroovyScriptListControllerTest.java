b820174e6826f9aa5149e101a2a87eacd6ec4337
Robby O'Connor <robby.oconnor@gmail.com>
2009-06-28 04:18:40 +0000

    facility data: some data and domain model changes.

---
+++ test/org/openmrs/module/groovy/web/controller/GroovyScriptListControllerTest.java
@@ -0,0 +1,27 @@
+package org.openmrs.module.groovy.web.controller;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+public class GroovyScriptListControllerTest {
+    @Before
+    public void setUp() {
+        // Add your code here
+    }
+
+    @After
+    public void tearDown() {
+        // Add your code here
+    }
+
+    @Test
+    public void testListForms() {
+        // Add your code here
+    }
+
+    @Test
+    public void testDelete() {
+        // Add your code here
+    }
+}
