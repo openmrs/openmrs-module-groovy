78b10d21d5e401dcfefeec6860eca1c9d378af2d
Michael Seaton <mseaton@pih.org>
2022-05-31 14:25:40 -0400

    GRV-40:  Groovy scripts fail to execute on running 2.5 instance

--- api/src/test/java/org/openmrs/module/groovy/api/GroovyModuleServiceTest.java
+++ api/src/test/java/org/openmrs/module/groovy/api/GroovyModuleServiceTest.java
@@ -11,18 +11,45 @@ package org.openmrs.module.groovy.api;
 
 import org.junit.Test;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.groovy.GroovyScript;
 import org.openmrs.module.groovy.api.service.GroovyService;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertNotNull;
 
 /**
  * Tests {@link {GroovyModuleService}}.
  */
-public class  GroovyModuleServiceTest extends BaseModuleContextSensitiveTest {
+public class GroovyModuleServiceTest extends BaseModuleContextSensitiveTest {
 	
 	@Test
 	public void shouldSetupContext() {
 		assertNotNull(Context.getService(GroovyService.class));
+	}
+
+	@Test
+	public void shouldSaveGroovyScript() {
+		GroovyScript groovyScript = new GroovyScript();
+		groovyScript.setName("Test Script");
+		groovyScript.setScript(getScript());
+		Context.getService(GroovyService.class).saveGroovyScript(groovyScript);
+		assertThat(groovyScript.getId(), notNullValue());
+	}
+
+	@Test
+	public void shouldExecuteGroovyScript() {
+		String[] eval = Context.getService(GroovyService.class).evaluate(getScript());
+		assertThat(eval.length, equalTo(3));
+		assertThat(eval[1].trim(), equalTo("2"));
+	}
+
+	public String getScript() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("p = patient.getPatientByUuid(\"da7f524f-27ce-4bb2-86d6-6d1d05312bd5\");");
+		sb.append("println p.getId();");
+		return sb.toString();
 	}
 }
